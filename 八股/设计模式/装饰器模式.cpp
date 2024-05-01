a78708b0ccbed35ee8f29a2e31a5bd1bf14e51fe
zhangziqi <zhangziqi2000913@163.com>
2024-05-02 03:27:54 +0800

    fix

--- 八股/设计模式/装饰器模式.cpp
+++ 八股/设计模式/装饰器模式.cpp
@@ -14,7 +14,7 @@ public:
     Hero() {}
     Hero(std::string name) : name(name) {}
     virtual std::string get_equipment() {
-        return "英雄: " + name + " 的装备： ";
+        return "英雄: " + name + " 的装备有: 手, ";
     }
 private:
     std::string name;
@@ -39,7 +39,9 @@ class Knife : public Decorator {
 public:
     Knife(Hero* hero) : Decorator(hero) {}
     std::string get_equipment() override {  // 装饰英雄的武器
-        return Decorator::get_equipment() + " 刀， ";
+        std::string cur_equip = Decorator::get_equipment();
+        cur_equip += " 刀, ";
+        return cur_equip;
     }
 };
 
@@ -47,17 +49,19 @@ class Gun : public Decorator {
 public:
     Gun(Hero* hero) : Decorator(hero) {}
     std::string get_equipment() override {  // 装饰英雄的武器
-        return Decorator::get_equipment() + " 枪， ";
+        std::string cur_equip = Decorator::get_equipment();
+        cur_equip += " 枪, ";
+        return cur_equip;
     }
 };
 
 int main() {
     Hero* iron_man = new Hero("iron man");
-    iron_man = new Knife(iron_man);
-    std::cout << iron_man->get_equipment() << std::endl;
+    auto iron_man_with_kinfe = new Knife(iron_man);
+    std::cout << iron_man_with_kinfe->get_equipment() << std::endl;
 
-    iron_man = new Gun(iron_man);
-    std::cout << iron_man->get_equipment() << std::endl;;
+    auto iron_man_all = new Gun(iron_man_with_kinfe);
+    std::cout << iron_man_all->get_equipment() << std::endl;;
 
     return 0;
 }
