cf6715b1f567c641468149ea4bc350f564fabd5e
zhangziqi <zhangziqi2000913@163.com>
2024-05-02 03:16:10 +0800

    add装饰器

---
+++ 八股/设计模式/装饰器模式.cpp
@@ -0,0 +1,63 @@
+/*
+    装饰器模式：Decorator
+    动态地给一个对象添加额外的职责，就增加功能来说，比生成子类更为灵活
+    把每个要装饰的功能放在单独的类中，让这个类包装它所要装饰的对象
+    有效地把类的核心职责和装饰功能区分开了，可以去除相关类中重复的装饰逻辑
+*/
+
+#include <iostream>
+#include <string>
+
+// 基类，被装饰的类
+class Hero {
+public:
+    Hero() {}
+    Hero(std::string name) : name(name) {}
+    virtual std::string get_equipment() {
+        return "英雄: " + name + " 的装备： ";
+    }
+private:
+    std::string name;
+};
+
+// 装饰器，对被装饰的类进行装饰
+class Decorator : public Hero {
+public:
+    Decorator(Hero* hero) : hero(hero) {}
+    std::string get_equipment() override {
+        if (hero != nullptr) {
+            return hero->get_equipment();
+        }
+        return "未找到对应英雄";
+    }
+protected:
+    Hero* hero;
+};
+
+// 功能类，装饰基类
+class Knife : public Decorator {
+public:
+    Knife(Hero* hero) : Decorator(hero) {}
+    std::string get_equipment() override {  // 装饰英雄的武器
+        return Decorator::get_equipment() + " 刀， ";
+    }
+};
+
+class Gun : public Decorator {
+public:
+    Gun(Hero* hero) : Decorator(hero) {}
+    std::string get_equipment() override {  // 装饰英雄的武器
+        return Decorator::get_equipment() + " 枪， ";
+    }
+};
+
+int main() {
+    Hero* iron_man = new Hero("iron man");
+    iron_man = new Knife(iron_man);
+    std::cout << iron_man->get_equipment() << std::endl;
+
+    iron_man = new Gun(iron_man);
+    std::cout << iron_man->get_equipment() << std::endl;;
+
+    return 0;
+}
