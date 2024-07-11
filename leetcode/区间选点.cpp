4bd414716c258947e07b9c73b2e0617e62dadb4f
zzq_win <zhangziqi2000913@163.com>
2024-07-12 02:07:59 +0800

    fix

--- leetcode/区间选点.cpp
+++ leetcode/区间选点.cpp
@@ -36,3 +36,36 @@ int main() {
     cout << res << endl;
     return 0;
 }
+
+/*
+    优化：只需记录右边界即可
+*/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<vector<int>> nums(n, vector<int>(2, 0));
+    for (int i = 0; i < n; ++ i) {
+        cin >> nums[i][0] >> nums[i][1];    // 输出
+    }
+    sort(nums.begin(), nums.end()); // 按左端点排序
+    int res = 1;    // 初始值为1，因为最后一个重合区间不会被记录，因此提前记录
+    int r = nums[0][1]; // 初始化第一个区间的右边界，用于寻找前n个区间的重合区间
+    for (int i = 1; i < n; ++ i) {
+        int& x = nums[i][0];
+        int& y = nums[i][1];
+        if (x > r) {    // 如果该区间的左端点已经超过当前重合区间的右端点，则该区间及之后不可能和之前的区间都重合
+            ++ res; // 因此当前重合区间中有一个点
+            r = y;
+        } else {    // 否则该区间可以和前面的区间有重合部分
+            r = min(r, y);
+        }
+    }
+    cout << res << endl;
+    return 0;
+}
